bb6d14b8ac6c79dfa43bbb66ced880518a5fc4f0
Proskuryakov Alesha <a.proskuryakov@propellerads.net>
2016-10-10 13:38:19 +0300

    add setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='wake_up',
+      version='0.1',
+      description='Wake up module',
+      author='alexeyka',
+      author_email='stack2008@gmail.com'
+      )
